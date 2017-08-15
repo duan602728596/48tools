a1ab164b784d97932cf6d267ad179a78441b32a2
duanhaochen <duanhaochen@126.com>
2017-08-15 13:27:57 +0800

    功能完成

--- app/src/modules/Index/index.js
+++ app/src/modules/Index/index.js
@@ -40,7 +40,7 @@ class Index extends Component{
   render(): Object{
     return(
       <div className={ style.body }>
-        <h1 className={ style.title }>48视频剪辑应援工具</h1>
+        <h1 className={ style.title }>48视应援工具</h1>
         <p className={ style.text }>
           本软件遵循
           <b>GNU General Public License v3.0</b>
@@ -66,23 +66,18 @@ class Index extends Component{
           </label>
         </div>
         <Row type="flex" align="top" justify="start">
-          {
-            this.props.test ?
-              (
-                <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
-                  <dl className={ style.linkGroup }>
-                    <dt className={ style.bNext }>
-                      <Link to="/LiveCatch" title="口袋48直播抓取">
-                        <img src={ require('./image/hty1.jpg') } alt="口袋48直播抓取" />
-                      </Link>
-                    </dt>
-                    <dd>
-                      <Link to="/LiveCatch">直播抓取</Link>
-                    </dd>
-                  </dl>
-                </Col>
-              ) : null
-          }
+          <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
+            <dl className={ style.linkGroup }>
+              <dt className={ style.bTest }>
+                <Link to="/LiveCatch" title="口袋48直播抓取">
+                  <img src={ require('./image/hty1.jpg') } alt="口袋48直播抓取" />
+                </Link>
+              </dt>
+              <dd>
+                <Link to="/LiveCatch">直播抓取</Link>
+              </dd>
+            </dl>
+          </Col>
           <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
             <dl className={ style.linkGroup }>
               <dt className={ style.bTest }>
@@ -95,23 +90,18 @@ class Index extends Component{
               </dd>
             </dl>
           </Col>
-          {
-            this.props.test ?
-              (
-                <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
-                  <dl className={ style.linkGroup }>
-                    <dt className={ style.bNext }>
-                      <Link to="/BiliBili" title="B站直播抓取">
-                        <img src={ require('./image/lyy1.jpg') } alt="B站直播抓取" />
-                      </Link>
-                    </dt>
-                    <dd>
-                      <Link to="/BiliBili">B站直播抓取</Link>
-                    </dd>
-                  </dl>
-                </Col>
-              ) : null
-          }
+          <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
+            <dl className={ style.linkGroup }>
+              <dt className={ style.bTest }>
+                <Link to="/BiliBili" title="B站直播抓取">
+                  <img src={ require('./image/lyy1.jpg') } alt="B站直播抓取" />
+                </Link>
+              </dt>
+              <dd>
+                <Link to="/BiliBili">B站直播抓取</Link>
+              </dd>
+            </dl>
+          </Col>
           <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
             <dl className={ style.linkGroup }>
               <dt className={ style.bTest }>
