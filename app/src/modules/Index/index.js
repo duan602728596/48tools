a26df0d68b4a1d648e848bf08d7a2cf75007c0ae
duanhaochen <duanhaochen@126.com>
2017-09-14 23:25:31 +0800

    添加新模块

--- app/src/modules/Index/index.js
+++ app/src/modules/Index/index.js
@@ -126,6 +126,18 @@ class Index extends Component{
               </dd>
             </dl>
           </Col>
+          <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
+            <dl className={ style.linkGroup }>
+              <dt className={ style.bTest }>
+                <Link to="/LiveDownload" title="公演录播下载">
+                  <img src={ require('./image/zmh1.jpg') } alt="公演录播下载" />
+                </Link>
+              </dt>
+              <dd>
+                <Link to="/LiveDownload">公演录播下载</Link>
+              </dd>
+            </dl>
+          </Col>
         </Row>
       </div>
     );
--- app/src/router/router.js
+++ app/src/router/router.js
@@ -2,21 +2,25 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import asyncModule from './asyncModule';
+/* 加载模块 */
 import Index from '../modules/Index/index';
-import LiveCatch from 'bundle-loader?lazy&name=livecatch!../modules/LiveCatch/Index/index';
+import LiveCatch from 'bundle-loader?lazy&name=livecatch&rely=../modules/LiveCatch/store/reducer!../modules/LiveCatch/Index/index';
 import LiveCatch_Option from 'bundle-loader?lazy&name=livecatch!../modules/LiveCatch/Option/index';
-import liveCatchReducer from 'bundle-loader?lazy&name=livecatch!../modules/LiveCatch/store/reducer';
 import PlayBackDownload from 'bundle-loader?lazy&name=playbackdownload!../modules/PlayBackDownload/Index/index';
 import PlayBackDownload_Detail from 'bundle-loader?lazy&name=playbackdownload!../modules/PlayBackDownload/Detail/index';
 import PlayBackDownload_List from 'bundle-loader?lazy&name=playbackdownload!../modules/PlayBackDownload/List/index';
-import playBackDownloadReducer from 'bundle-loader?lazy&name=playbackdownload!../modules/PlayBackDownload/store/reducer';
 import BiliBili from 'bundle-loader?lazy&name=bilibili!../modules/Bilibili/Index/index';
 import BiliBili_Option from 'bundle-loader?lazy&name=bilibili!../modules/Bilibili/Option/index';
-import biliBiliReducer from 'bundle-loader?lazy&name=bilibili!../modules/Bilibili/store/reducer';
 import Cut from 'bundle-loader?lazy&name=cut!../modules/Cut/Index/index';
-import cutReducer from 'bundle-loader?lazy&name=cut!../modules/Cut/store/render';
 import Wds from 'bundle-loader?lazy&name=wds!../modules/Wds/Index/index';
+import LiveDownload from 'bundle-loader?lazy&name=livedownload!../modules/LiveDownload/Index/index';
+/* 加载reducer */
+import liveCatchReducer from 'bundle-loader?lazy&name=livecatch!../modules/LiveCatch/store/reducer';
+import playBackDownloadReducer from 'bundle-loader?lazy&name=playbackdownload!../modules/PlayBackDownload/store/reducer';
+import biliBiliReducer from 'bundle-loader?lazy&name=bilibili!../modules/Bilibili/store/reducer';
+import cutReducer from 'bundle-loader?lazy&name=cut!../modules/Cut/store/reducer';
 import wdsReducer from 'bundle-loader?lazy&name=wds!../modules/Wds/store/reducer';
+import liveDownloadReducer from 'bundle-loader?lazy&name=livedownload!../modules/LiveDownload/store/reducer';
 
 /* 路由模块 */
 class Router extends Component{
@@ -55,6 +59,8 @@ class Router extends Component{
           <Route path="/Cut" component={ asyncModule(Cut, cutReducer) } exact />
           {/* 微打赏 */}
           <Route path="/Wds" component={ asyncModule(Wds, wdsReducer) } exact />
+          {/* 公演录播下载 */}
+          <Route path="/LiveDownload" component={ asyncModule(LiveDownload, liveDownloadReducer) } exact />
         </Switch>
       </div>
     );
