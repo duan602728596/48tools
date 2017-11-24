bf29c4f1a7a379c6fb367d7046b3cc7a819c5543
duanhaochen <duanhaochen@126.com>
2017-11-25 00:50:43 +0800

    修改

--- app/src/router/asyncModule.js
+++ app/src/router/asyncModule.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import Bundle from './bundle';
 
-/***
+/**
  * 异步加载、注入模块和reducer
  * @param { Function } module               需要异步注入的模块
  * @param { Function | null } asyncReducer  需要异步注入的reducer
