6db2c2d7dbaa8dd722bb28fb9dc2e7c1be5c0dcd
duanhaochen <duanhaochen@126.com>
2017-12-15 13:51:49 +0800

    修改css in js的输出名称

--- app/config/css.config.js
+++ app/config/css.config.js
@@ -1,6 +1,6 @@
 /* css-loader 配置 */
 // css in js 的输出名称
-const name = '[name]__[local]___[hash:base64:15]';
+const name = '[path][name]__[local]___[hash:base64:15]';
 
 module.exports = {
   path: 'css-loader',
