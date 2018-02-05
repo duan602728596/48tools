b729ed1c3d708f2e5f2ecc88fd8f3259c9e802c9
duanhaochen <duanhaochen@126.com>
2018-02-05 18:03:23 +0800

    修改css-loader配置

--- app/config/css.config.js
+++ app/config/css.config.js
@@ -1,6 +1,20 @@
 /* css-loader 配置 */
-// css in js 的输出名称
-const name = '[path][name]__[local]___[hash:base64:15]';
+const process = require('process');
+
+function output(env){
+  switch(env){
+    case 'development': // 开发环境
+      return '[path][name]__[local]___[hash:base64:15]';
+    case 'production':  // 生产环境
+      return '_[hash:base64:5]';
+  }
+}
+
+// 根据当前环境配置css-in-js的name
+// development
+// production
+const env = process.env.NODE_ENV;
+const name = output(env);
 
 module.exports = {
   path: 'css-loader',
