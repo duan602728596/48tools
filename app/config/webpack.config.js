c0e062e4379b5f324f8b1807a02323d86b5f03fc
段昊辰 <duanhaochen@126.com>
2018-02-28 20:39:13 +0800

    Update webpack.config.js

--- app/config/webpack.config.js
+++ app/config/webpack.config.js
@@ -35,7 +35,7 @@ function config(options){
         },
         { // sass
           test: /^.*\.sass$/,
-          use: ['style-loader', cssConfig, postcssConfig, sassConfig],
+          use: ['style-loader', cssConfig, postcssConfig, sassConfig]
         },
         { // css
           test: /^.*\.css$/,
