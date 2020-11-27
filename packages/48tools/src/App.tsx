f92af5a74c553effe589ac0386272e4af5ec34f5
段昊辰 <duanhaochen@126.com>
2020-11-27 16:14:28 +0800

    refactor: 移除初始化数据库的代码

--- packages/48tools/src/App.tsx
+++ packages/48tools/src/App.tsx
@@ -1,18 +1,13 @@
-import { useEffect, ReactElement } from 'react';
+import { ReactElement } from 'react';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale-provider/zh_CN';
 import { storeFactory } from './store/store';
 import Routers from './router/Routers';
-import dbInit from './function/dbInit/dbInit';
 
 /* App */
 function App(props: {}): ReactElement {
-  useEffect(function(): void {
-    dbInit();
-  }, []);
-
   return (
     <Provider store={ storeFactory() }>
       <ConfigProvider locale={ zhCN }>
