d6a6d092e1933e013bd89e389ad19c559b7a2d17
段昊辰 <duanhaochen@126.com>
2020-12-02 18:28:41 +0800

    feat: 显示房间信息

--- packages/48tools/src/pages/48/Player/PlayerApp.tsx
+++ packages/48tools/src/pages/48/Player/PlayerApp.tsx
@@ -1,8 +1,9 @@
 import * as querystring from 'querystring';
 import type { ParsedUrlQuery } from 'querystring';
-import { useState, useMemo, ReactElement, Dispatch as D, SetStateAction as S } from 'react';
-import { ConfigProvider } from 'antd';
+import { useState, useEffect, useMemo, ReactElement, ReactNodeArray, Dispatch as D, SetStateAction as S } from 'react';
+import { ConfigProvider, Avatar } from 'antd';
 import zhCN from 'antd/es/locale-provider/zh_CN';
+import style from './playerApp.sass';
 import { requestLiveRoomInfo } from '../services/services';
 import type { LiveRoomInfo } from '../types';
 
@@ -12,6 +13,8 @@ interface Search {
   liveId: string;
   id: string;
 }
+
+const SOURCE_HOST: string = 'https://source3.48.cn/'; // 静态文件地址
 
 /* PlayerApp */
 function PlayerApp(props: {}): ReactElement {
@@ -27,10 +30,42 @@ function PlayerApp(props: {}): ReactElement {
   }, []);
   const [info, setInfo]: [LiveRoomInfo | undefined, D<S<LiveRoomInfo | undefined>>] = useState(undefined); // 直播信息
 
+  // 请求直播间信息
+  async function getLiveRoomInfo(): Promise<void> {
+    try {
+      const res: LiveRoomInfo = await requestLiveRoomInfo(search.liveId);
+
+      setInfo(res);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // 渲染小偶像信息
+  function infoRender(): ReactNodeArray | null {
+    if (info) {
+      const { content }: LiveRoomInfo = info;
+
+      return [
+        <Avatar key="avatar" src={ `${ SOURCE_HOST }${ content.user.userAvatar }` } />,
+        <span key="username" className={ style.user }>{ content.user.userName }</span>
+      ];
+    } else {
+      return null;
+    }
+  }
+
+  useEffect(function(): void {
+    getLiveRoomInfo();
+  }, []);
+
   return (
     <ConfigProvider locale={ zhCN }>
-      <div>
-
+      <div className={ style.content }>
+        <header className={ style.header }>
+          <h1 className={ style.title }>{ search.title }</h1>
+          { infoRender() }
+        </header>
       </div>
     </ConfigProvider>
   );
