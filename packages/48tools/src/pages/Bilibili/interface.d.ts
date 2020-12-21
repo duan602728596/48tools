48fa7a56ba6bdfdbeb5e3f4525841cbb86f24c1b
段昊辰 <duanhaochen@126.com>
2020-12-21 19:33:13 +0800

    feat: 直播接口地址

--- packages/48tools/src/pages/Bilibili/interface.d.ts
+++ packages/48tools/src/pages/Bilibili/interface.d.ts
@@ -15,3 +15,25 @@ export interface AudioInfo {
   };
   message: string;
 }
+
+// 直播间信息
+export interface RoomInit {
+  code: number;
+  msg: string;
+  message: string;
+  data: {
+    room_id: number;
+    short_id: number;
+    uid: number;
+    live_status: number;
+  };
+}
+
+// 直播间地址信息
+export interface RoomPlayUrl {
+  code: number;
+  message: string;
+  data: {
+    durl: Array<{ url: string }>;
+  };
+}
---
+++ packages/48tools/src/pages/Bilibili/services/live.ts
@@ -0,0 +1,26 @@
+import got, { Response } from 'got';
+import type { RoomInit, RoomPlayUrl } from '../interface';
+
+/**
+ * 获取直播间的初始信息
+ * @param { string } roomId
+ */
+export async function requestRoomInitData(roomId: string): Promise<RoomInit> {
+  const res: Response<RoomInit> = await got(`https://api.live.bilibili.com/room/v1/Room/room_init?id=${ roomId }`, {
+    responseType: 'json'
+  });
+
+  return res.body;
+}
+
+/**
+ * 获取直播间的直播地址
+ * @param { string } roomId
+ */
+export async function requestRoomPlayerUrl(roomId: string): Promise<RoomPlayUrl> {
+  const res: Response<RoomPlayUrl> = await got(`https://api.live.bilibili.com/room/v1/Room/playUrl?cid=${ roomId }&qn=10000&platform=web`, {
+    responseType: 'json'
+  });
+
+  return res.body;
+}
