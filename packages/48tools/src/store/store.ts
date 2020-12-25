c24cceb4aaee1b2034205da9bd847fc7b39d089b
段昊辰 <duanhaochen@126.com>
2020-12-25 15:24:06 +0800

    refactor: 调整store的导出

--- packages/48tools/src/store/store.ts
+++ packages/48tools/src/store/store.ts
@@ -7,20 +7,20 @@ const reducer: Reducer = createReducer({});
 const asyncReducers: ReducersMapObject = {}; // 异步的reducers
 
 /* store */
-const store: Store = {} as Store;
+let store: Store;
 
 export function storeFactory(initialState: DeepPartial<any> = {}): Store {
   // 避免热替换导致redux的状态丢失
-  if (Object.keys(store).length === 0) {
+  if (!store) {
     /* store */
-    Object.assign(store, configureStore({
+    store = configureStore({
       reducer,
       preloadedState: initialState,
       middleware: getDefaultMiddleware({
         immutableCheck: ignoreOptions,
         serializableCheck: ignoreOptions
       })
-    }));
+    });
   }
 
   return store;
@@ -40,5 +40,3 @@ export function injectReducers(asyncReducer: ReducersMapObject): void {
   // 异步注入reducer
   store.replaceReducer(createReducer(asyncReducers));
 }
-
-export default store;
