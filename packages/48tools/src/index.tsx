d3fdf532011ef6a425137c5a47c9371d9f678f58
段昊辰 <duanhaochen@126.com>
2020-12-26 12:03:00 +0800

    refactor: 调整初始化渲染

--- packages/48tools/src/index.tsx
+++ packages/48tools/src/index.tsx
@@ -1,11 +1,22 @@
 import { render } from 'react-dom';
-import App from './App';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale-provider/zh_CN';
+import { storeFactory } from './store/store';
+import Routers from './router/Routers';
 import { netMediaServerInit } from './utils/nodeMediaServer/nodeMediaServer';
 import dbInit from './utils/idb/dbInit';
 
 /* app */
 render(
-  <App />,
+  <Provider store={ storeFactory() }>
+    <ConfigProvider locale={ zhCN }>
+      <HashRouter>
+        <Routers />
+      </HashRouter>
+    </ConfigProvider>
+  </Provider>,
   document.getElementById('app')
 );
 
