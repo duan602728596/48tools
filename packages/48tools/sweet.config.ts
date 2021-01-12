3790da3a02c762cf16dd75688cce4c2c232b5033
段昊辰 <duanhaochen@126.com>
2021-01-12 09:44:07 +0800

    build: 修改exclude

--- packages/48tools/sweet.config.ts
+++ packages/48tools/sweet.config.ts
@@ -57,7 +57,7 @@ export default function(info: object): { [key: string]: any } {
     js: {
       ecmascript: true,
       plugins,
-      exclude: /node_modules|NIM_Web_SDK/i
+      exclude: /node_modules/i
     },
     ts: {
       configFile: isDev ? 'tsconfig.json' : 'tsconfig.prod.json',
