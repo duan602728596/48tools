8fe7fa0d5851247f7f7a358d847ac86e6592093a
段昊辰 <duanhaochen@126.com>
2021-05-05 10:59:53 +0800

    ci: 添加cjs的检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -96,7 +96,7 @@ module.exports = {
       }
     },
     {
-      files: ['*.js', '*.jsx', '*.mjs'],
+      files: ['*.js', '*.jsx', '*.mjs', '*.cjs'],
       parser: '@babel/eslint-parser',
       parserOptions: {
         requireConfigFile: false,
