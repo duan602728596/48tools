24c7a19ab1ffad9889062b23be6a2bf8794bd8cd
段昊辰 <duanhaochen@126.com>
2021-07-16 13:11:13 +0800

    refactor: 提取到通用方法中

--- packages/48tools/src/pages/48/services/pocket48.ts
+++ packages/48tools/src/pages/48/services/pocket48.ts
@@ -2,30 +2,10 @@ import { promisify } from 'util';
 import { pipeline } from 'stream';
 import * as fs from 'fs';
 import got, { Response as GotResponse } from 'got';
-import { rStr } from '../../../utils/utils';
+import { createHeaders } from '../../../utils/snh48';
 import type { LiveData, LiveRoomInfo } from './interface';
 
 const pipelineP: (stream1: NodeJS.ReadableStream, stream2: NodeJS.WritableStream) => Promise<void> = promisify(pipeline);
-
-/* 创建请求头 */
-function createHeaders(): { [key: string]: string } {
-  return {
-    'Content-Type': 'application/json;charset=utf-8',
-    appInfo: JSON.stringify({
-      vendor: 'apple',
-      deviceId: `${ rStr(8) }-${ rStr(4) }-${ rStr(4) }-${ rStr(4) }-${ rStr(12) }`,
-      appVersion: '6.0.16',
-      appBuild: '200701',
-      osVersion: '13.5.1',
-      osType: 'ios',
-      deviceName: 'iPhone XR',
-      os: 'ios'
-    }),
-    'User-Agent': 'PocketFans201807/6.0.16 (iPhone; iOS 13.5.1; Scale/2.00)',
-    'Accept-Language': 'zh-Hans-AW;q=1',
-    Host: 'pocketapi.48.cn'
-  };
-}
 
 /**
  * 获取单个直播间的信息
---
+++ packages/48tools/src/utils/snh48.ts
@@ -0,0 +1,21 @@
+import { rStr } from './utils';
+
+/* 创建请求头 */
+export function createHeaders(): { [key: string]: string } {
+  return {
+    'Content-Type': 'application/json;charset=utf-8',
+    appInfo: JSON.stringify({
+      vendor: 'apple',
+      deviceId: `${ rStr(8) }-${ rStr(4) }-${ rStr(4) }-${ rStr(4) }-${ rStr(12) }`,
+      appVersion: '6.0.16',
+      appBuild: '200701',
+      osVersion: '13.5.1',
+      osType: 'ios',
+      deviceName: 'iPhone XR',
+      os: 'ios'
+    }),
+    'User-Agent': 'PocketFans201807/6.0.16 (iPhone; iOS 13.5.1; Scale/2.00)',
+    'Accept-Language': 'zh-Hans-AW;q=1',
+    Host: 'pocketapi.48.cn'
+  };
+}
