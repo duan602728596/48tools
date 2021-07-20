aa8bc014fd838f01c79212a44e758bc3043da3b7
段昊辰 <duanhaochen@126.com>
2021-07-20 20:59:24 +0800

    feat: 由于getDefaultMiddleware已废弃，middleware修改为callback的形式创建

--- packages/48tools/src/store/store.ts
+++ packages/48tools/src/store/store.ts
@@ -2,12 +2,31 @@
 import {
   configureStore,
   combineReducers,
-  getDefaultMiddleware,
   Reducer,
   Store,
-  DeepPartial
+  DeepPartial,
+  ImmutableStateInvariantMiddlewareOptions,
+  SerializableStateInvariantMiddlewareOptions,
+  MiddlewareArray
 } from '@reduxjs/toolkit';
+import type { CurriedGetDefaultMiddleware, ThunkMiddlewareFor } from '@reduxjs/toolkit/src/getDefaultMiddleware';
+import type { Middleware } from 'redux';
 import { reducersMapObject, ignoreOptions } from './reducers';
+
+interface ThunkOptions<E = any> {
+  extraArgument: E;
+}
+
+interface GetDefaultMiddlewareOptions {
+  thunk?: boolean | ThunkOptions;
+  immutableCheck?: boolean | ImmutableStateInvariantMiddlewareOptions;
+  serializableCheck?: boolean | SerializableStateInvariantMiddlewareOptions;
+}
+
+type MiddlewareCbReturn = MiddlewareArray<
+  Middleware<{}, any>
+  | ThunkMiddlewareFor<any, GetDefaultMiddlewareOptions>
+>;
 
 /* reducer列表 */
 const reducer: Reducer = combineReducers(reducersMapObject);
@@ -22,10 +41,12 @@ export function storeFactory(initialState: DeepPartial<any> = {}): Store {
     store = configureStore({
       reducer,
       preloadedState: initialState,
-      middleware: getDefaultMiddleware({
-        immutableCheck: ignoreOptions,
-        serializableCheck: ignoreOptions
-      })
+      middleware(getDefaultMiddleware: CurriedGetDefaultMiddleware): MiddlewareCbReturn {
+        return getDefaultMiddleware<GetDefaultMiddlewareOptions>({
+          immutableCheck: ignoreOptions,
+          serializableCheck: ignoreOptions
+        });
+      }
     });
   }
 
