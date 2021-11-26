a3cd196f84e418581a9fc211e8f58d6af6470e90
段昊辰 <duanhaochen@126.com>
2021-11-26 10:48:42 +0800

    feat: 修改暗黑模式文件的生成方式

--- packages/darkTheme/src/commentary.js
+++
@@ -1,6 +0,0 @@
-/* 生成注释 */
-function commentary() {
-  return '/*! @48tools 暗黑模式css文件 !*/';
-}
-
-export default commentary;
--- packages/darkTheme/src/index.js
+++ packages/darkTheme/src/index.js
@@ -2,11 +2,9 @@ import path from 'node:path';
 import { promises as fsP } from 'node:fs';
 import { promisify } from 'node:util';
 import glob from 'glob';
-import _ from 'lodash';
 import { metaHelper, moduleExists } from '@sweet-milktea/utils';
-import matchComponents from './matchComponents.js';
-import { lessFile, lessRender } from './lessFile.js';
-import commentary from './commentary.js';
+import parser from './parser.js';
+import lessCode from './lessCode.js';
 
 const globPromise = promisify(glob);
 const { __dirname } = metaHelper(import.meta.url);
@@ -15,26 +13,13 @@ const { __dirname } = metaHelper(import.meta.url);
 async function main() {
   // 获取所有的tsx文件
   const cwd48tools = path.join(__dirname, '../../48tools/src');
-  const files = await globPromise('**/*.tsx', {
-    cwd: cwd48tools
-  });
+  const files = await globPromise('**/*.tsx', { cwd: cwd48tools });
 
-  // 提取组件名称
-  let allComponents = ['Pagination', 'Empty'];
-
-  for (const file of files) {
-    const components = await matchComponents(cwd48tools, file);
-
-    if (components) {
-      allComponents.push(...components);
-    }
-  }
-
-  allComponents = _.uniq(allComponents);
+  // 查找antd组件
+  const antdComponents = await parser(cwd48tools, files);
 
   // 生成less文件
-  const componentsLessFiles = lessFile(allComponents);
-  const css = await lessRender(componentsLessFiles);
+  const css = await lessCode(antdComponents);
   const distDir = path.join(__dirname, '../dist');
 
   // 代码高亮的暗黑主题样式
@@ -42,7 +27,7 @@ async function main() {
   const hljsCss = await fsP.readFile(hljsPath, { encoding: 'utf8' });
 
   await fsP.mkdir(distDir);
-  await fsP.writeFile(path.join(distDir, 'dark-theme.css'), `${ commentary() }\n${ css }\n${ hljsCss }`);
+  await fsP.writeFile(path.join(distDir, 'dark-theme.css'), `/*! @48tools 暗黑模式css文件 !*/\n${ css }\n${ hljsCss }`);
 }
 
 main();
