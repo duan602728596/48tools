6c373cd865a8a9c6bb6d7bd9718a6a1c5a37d7d3
段昊辰 <duanhaochen@126.com>
2022-01-31 17:48:11 +0800

    build: 修改编译配置

--- packages/website/sweet.config.ts
+++ packages/website/sweet.config.ts
@@ -1,27 +1,30 @@
 import * as process from 'node:process';
 import babel from 'vite-plugin-babel';
-import type { InlineConfig, Plugin } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import type { InlineConfig } from 'vite';
 
 const isDev: boolean = process.env.NODE_ENV === 'development';
-const plugins: Array<Plugin | Plugin[]> = [];
+
 const vite: InlineConfig = {};
 
 if (!isDev) {
-  plugins.push(
+  vite.plugins = [
+    createHtmlPlugin({
+      minify: true
+    }),
     babel({
       babelConfig: {
         babelrc: false,
         configFile: false,
-        plugin: [['transform-react-remove-prop-types', { mode: 'remove', removeImport: true }]]
+        plugins: [['transform-react-remove-prop-types', { mode: 'remove', removeImport: true }]]
       }
     })
-  );
+  ];
 
   vite.build = { sourcemap: true };
 }
 
 export default {
   frame: 'react',
-  plugins,
   vite
 };
