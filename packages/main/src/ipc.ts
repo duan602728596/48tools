1b48ec4c2024c15543c10d7fd5891ce7dcce495c
duanhaochen <duanhaochen@126.com>
2022-03-03 14:07:55 +0800

    refactor: 使用enum定义通信类型

--- packages/main/src/ipc.ts
+++ packages/main/src/ipc.ts
@@ -5,17 +5,19 @@ import weiboLogin from './function/weiboLogin';
 import { nodeMediaServerInit, NODE_MEDIA_SERVER_CHANNEL } from './nodeMediaServer/nodeMediaServer';
 
 // 通信类型
-const DEVELOP_TOOLS_CHANNEL: string = 'developer-tools';
-const PLAYER_HTML_CHANNEL: string = 'player.html';
-const PLAYER_DEVELOPER_TOOLS_CHANNEL: string = 'player-developer-tools';
-const WEIBO_LOGIN_CHANNEL: string = 'weibo-login';
+enum IpcListenerType {
+  DEVELOP_TOOLS_CHANNEL = 'developer-tools',
+  PLAYER_HTML_CHANNEL = 'player.html',
+  PLAYER_DEVELOPER_TOOLS_CHANNEL = 'player-developer-tools',
+  WEIBO_LOGIN_CHANNEL = 'weibo-login'
+}
 
-/* 移除所有监听的通信 */
+// 移除所有监听的通信
 const removeListenerChannel: Array<string> = [
-  DEVELOP_TOOLS_CHANNEL,
-  PLAYER_HTML_CHANNEL,
-  PLAYER_DEVELOPER_TOOLS_CHANNEL,
-  WEIBO_LOGIN_CHANNEL,
+  IpcListenerType.DEVELOP_TOOLS_CHANNEL,
+  IpcListenerType.PLAYER_HTML_CHANNEL,
+  IpcListenerType.PLAYER_DEVELOPER_TOOLS_CHANNEL,
+  IpcListenerType.WEIBO_LOGIN_CHANNEL,
   NATIVE_THEME_CHANGE_CHANNEL,
   NODE_MEDIA_SERVER_CHANNEL
 ];
@@ -29,12 +31,12 @@ export function removeIpc(): void {
 /* ipc通信 */
 export function ipc(win: BrowserWindow): void {
   // 打开开发者工具
-  ipcMain.on(DEVELOP_TOOLS_CHANNEL, function(event: IpcMainEvent): void {
+  ipcMain.on(IpcListenerType.DEVELOP_TOOLS_CHANNEL, function(event: IpcMainEvent): void {
     win.webContents.openDevTools();
   });
 
   // 获取其他路由的文件的绝对路径
-  ipcMain.on(PLAYER_HTML_CHANNEL, function(event: IpcMainEvent, title: string, query: string): void {
+  ipcMain.on(IpcListenerType.PLAYER_HTML_CHANNEL, function(event: IpcMainEvent, title: string, query: string): void {
     openPlayerHtml(title, query);
   });
 
@@ -43,7 +45,7 @@ export function ipc(win: BrowserWindow): void {
    * @param { IpcMainEvent } event
    * @param { string } pid: 当前窗口的唯一id
    */
-  ipcMain.on(PLAYER_DEVELOPER_TOOLS_CHANNEL, function(event: IpcMainEvent, pid: string): void {
+  ipcMain.on(IpcListenerType.PLAYER_DEVELOPER_TOOLS_CHANNEL, function(event: IpcMainEvent, pid: string): void {
     playerWindowMaps.get(pid)?.webContents.openDevTools();
   });
 
@@ -51,7 +53,7 @@ export function ipc(win: BrowserWindow): void {
   ipcTheme();
 
   // 微博登陆
-  ipcMain.on(WEIBO_LOGIN_CHANNEL, function(event: IpcMainEvent): void {
+  ipcMain.on(IpcListenerType.WEIBO_LOGIN_CHANNEL, function(event: IpcMainEvent): void {
     weiboLogin(win);
   });
 
