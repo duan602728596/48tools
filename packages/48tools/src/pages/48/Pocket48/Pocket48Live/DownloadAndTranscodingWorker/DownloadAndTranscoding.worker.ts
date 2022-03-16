d2ac6670fa1b8acdd03b03629fe959c0c8252540
段昊辰 <duanhaochen@126.com>
2022-03-16 22:56:16 +0800

    refactor: 添加todo

--- packages/48tools/src/pages/48/Pocket48/Pocket48Live/DownloadAndTranscodingWorker/DownloadAndTranscoding.worker.ts
+++ packages/48tools/src/pages/48/Pocket48/Pocket48Live/DownloadAndTranscodingWorker/DownloadAndTranscoding.worker.ts
@@ -13,6 +13,9 @@ let command: FfmpegCommand;
 /**
  * 转码并下载
  * https://stackoverflow.com/questions/16658873/how-to-minimize-the-delay-in-a-live-streaming-with-ffmpeg
+ * TODO: 连麦错误原因在于连麦和非连麦录制时DTS不统一，编码采用开始时的信息，导致花屏。
+ *       修复方式为每次录制都重新编码，不过最后的视频会有错误，错误信息
+ *       [DTS discontinuity in stream 0: packet 3 with DTS 135001, packet 4 with DTS 144000]
  */
 function download(workerData: WorkerEventData): void {
   const { ffmpeg, playStreamPath, filePath }: WorkerEventData = workerData;
