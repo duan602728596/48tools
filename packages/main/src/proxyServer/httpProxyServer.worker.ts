8dea1ed9c5fe5219baef71cc86198f3443be78b6
段昊辰 <duanhaochen@126.com>
2022-08-10 21:08:33 +0800

    perf: 优化代码

--- packages/main/src/proxyServer/httpProxyServer.worker.ts
+++ packages/main/src/proxyServer/httpProxyServer.worker.ts
@@ -1,16 +1,16 @@
 import * as https from 'node:https';
 import * as http from 'node:http';
-import type { Server, IncomingMessage, ServerResponse } from 'node:http';
+import type { IncomingMessage, ServerResponse, OutgoingHttpHeaders } from 'node:http';
 import { workerData } from 'node:worker_threads';
 
 const baseUrl: string = `http://localhost:${ workerData.port }`;
 
 /**
- * 官网公演录播的ts的下载
  * @param { URL } urlParse
  * @param { ServerResponse } httpResponse
+ * @param { OutgoingHttpHeaders } headers
  */
-function ts48(urlParse: URL, httpResponse: ServerResponse): void {
+function tsResponseHandle(urlParse: URL, httpResponse: ServerResponse, headers: OutgoingHttpHeaders): void {
   const tsUrl: string | null = urlParse.searchParams.get('url');
 
   if (!tsUrl) return;
@@ -18,53 +18,30 @@ function ts48(urlParse: URL, httpResponse: ServerResponse): void {
   const deTsUrl: string = decodeURIComponent(tsUrl);
   const deTsUrlParse: URL = new URL(deTsUrl);
 
-  (deTsUrlParse.protocol === 'https:' ? https : http).get(deTsUrl, {
-    headers: {
-      Host: 'ts.48.cn',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)'
-        + ' Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62'
-    }
-  }, function(response: IncomingMessage): void {
-    httpResponse.setHeader('Content-type', 'video/mp2ts');
-    response.pipe(httpResponse);
-  });
-}
-
-/**
- * 口袋48录播的ts的下载
- * @param { URL } urlParse
- * @param { ServerResponse } httpResponse
- */
-function cychengyuanVod48(urlParse: URL, httpResponse: ServerResponse): void {
-  const tsUrl: string | null = urlParse.searchParams.get('url');
-
-  if (!tsUrl) return;
-
-  const deTsUrl: string = decodeURIComponent(tsUrl);
-  const deTsUrlParse: URL = new URL(deTsUrl);
-
-  (deTsUrlParse.protocol === 'https:' ? https : http).get(deTsUrl, {
-    headers: {
-      Host: 'cychengyuan-vod.48.cn',
-      'User-Agent': 'SNH48 ENGINE'
-    }
-  }, function(response: IncomingMessage): void {
+  (deTsUrlParse.protocol === 'https:' ? https : http).get(deTsUrl, { headers }, function(response: IncomingMessage): void {
     httpResponse.setHeader('Content-type', 'video/mp2ts');
     response.pipe(httpResponse);
   });
 }
 
 /* 开启代理服务，加载ts文件 */
-const server: Server = http.createServer(function(httpRequest: IncomingMessage, httpResponse: ServerResponse): void {
+http.createServer(function(httpRequest: IncomingMessage, httpResponse: ServerResponse): void {
   if (!httpRequest.url) return;
 
   const urlParse: URL = new URL(httpRequest.url, baseUrl);
 
   if (urlParse.pathname === '/proxy/ts48' ) {
-    ts48(urlParse, httpResponse);
+    // 官网公演录播的ts的下载
+    tsResponseHandle(urlParse, httpResponse, {
+      Host: 'ts.48.cn',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)'
+        + ' Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62'
+    });
   } else if (urlParse.pathname === '/proxy/cychengyuan-vod48' ) {
-    cychengyuanVod48(urlParse, httpResponse);
+    // 口袋48录播的ts的下载
+    tsResponseHandle(urlParse, httpResponse, {
+      Host: 'cychengyuan-vod.48.cn',
+      'User-Agent': 'SNH48 ENGINE'
+    });
   }
-});
-
-server.listen(workerData.port);
+}).listen(workerData.port);
