1b2ebc62c2f030c34db5ce53faab41552d836802
段昊辰 <duanhaochen@126.com>
2022-08-15 22:58:25 +0800

    build: 修改main的编译方式

--- packages/main/gulpfile.mjs
+++ packages/main/gulpfile.mjs
@@ -1,43 +1,9 @@
-import process from 'node:process';
-import path from 'node:path';
 import gulp from 'gulp';
 import terser from 'gulp-terser';
-import typescript from 'gulp-typescript';
-import changed from 'gulp-changed';
-import plumber from 'gulp-plumber';
-import { requireJson, metaHelper } from '@sweet-milktea/utils';
 
-const { __dirname } = metaHelper(import.meta.url);
-const baseTypescriptConfig = await requireJson(path.join(__dirname, '../../tsconfig.json'));
-const tsConfig = await requireJson(path.join(__dirname, './tsconfig.json'));
-
-const isDevelopment = process.env.NODE_ENV === 'development';
-const isWatch = process.env.WATCH;
-
-function devTsProject() {
-  const result = gulp.src('src/**/*.{ts,tsx}')
-    .pipe(changed('lib'))
-    .pipe(plumber())
-    .pipe(typescript({
-      ...baseTypescriptConfig.compilerOptions,
-      ...tsConfig.compilerOptions
-    }));
-
-  return result.js.pipe(gulp.dest('lib'));
-}
-
-function watch() {
-  gulp.watch('src/**/*.{ts,tsx}', devTsProject);
-}
-
-function tsProject() {
-  const result = gulp.src('src/**/*.{ts,tsx}')
-    .pipe(typescript({
-      ...baseTypescriptConfig.compilerOptions,
-      ...tsConfig.compilerOptions
-    }));
-
-  return result.js
+/* 代码压缩 */
+function minifyCode() {
+  return gulp.src('.lib.mid/**/*.{js,cjs,mjs}')
     .pipe(terser({
       ecma: 2020,
       module: true
@@ -45,6 +11,4 @@ function tsProject() {
     .pipe(gulp.dest('lib'));
 }
 
-export default isDevelopment
-  ? (isWatch ? gulp.series(devTsProject, watch) : devTsProject)
-  : tsProject;
+export default minifyCode;
