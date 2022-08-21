9a67ce8e8eb5aea7aac8e6eccc56d0703ad7a381
段昊辰 <duanhaochen@126.com>
2022-08-21 20:06:47 +0800

    refactor: 添加缺失的类型

--- packages/test/src/tests/bilibili/bilibiliDownload.test.ts
+++ packages/test/src/tests/bilibili/bilibiliDownload.test.ts
@@ -149,7 +149,7 @@ export function callback(): void {
     await app.win.type('#spaceId', userId.toString());
     await app.win.click('.ant-modal-body .ant-form .ant-btn');
     await app.win.waitForTimeout(1_500); // 等待查询结果
-    await app.win.waitForFunction(() => document.querySelectorAll('.ant-spin').length === 0);
+    await app.win.waitForFunction((): boolean => document.querySelectorAll('.ant-spin').length === 0);
     await app.win.waitForTimeout(1_000);
 
     // 点击搜索视频详细数据
@@ -157,7 +157,7 @@ export function callback(): void {
 
     await seeVideo.nth(0).click();
     await app.win.waitForTimeout(1_500); // 等待查询结果
-    await app.win.waitForFunction(() => document.querySelectorAll('.ant-spin').length === 0);
+    await app.win.waitForFunction((): boolean => document.querySelectorAll('.ant-spin').length === 0);
     await app.win.waitForTimeout(1_000);
 
     // 添加到下载列表
