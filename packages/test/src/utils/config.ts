24d938a7a4028a720ac945b9b653af3a882600c1
段昊辰 <duanhaochen@126.com>
2022-08-21 16:27:22 +0800

    test: 添加扩展名

--- packages/test/src/utils/config.ts
+++ packages/test/src/utils/config.ts
@@ -1,10 +1,11 @@
 import path from 'node:path';
+import os from 'node:os';
 import { metaHelper } from '@sweet-milktea/utils';
 
 const { __dirname }: { __dirname: string } = metaHelper(import.meta.url);
 
 // 测试用ffmpeg的文件地址
-export const ffmpegPath: string = path.join(__dirname, '../../ffmpeg/bin/ffmpeg');
+export const ffmpegPath: string = path.join(__dirname, `../../ffmpeg/bin/ffmpeg${ os.platform() === 'win32' ? '.exe' : '' }`);
 
 export const testDir: string = path.join(__dirname, '../../dist');
 
