9b7bf9d78e6e15456c055712cf4847d7a004d6ef
段昊辰 <duanhaochen@126.com>
2023-01-25 23:42:37 +0800

    refactor: 删除废弃的微博登录实现方式

--- packages/48tools/src/functionalComponents/WeiboLogin/services/interface.d.ts
+++ packages/48tools/src/functionalComponents/WeiboLogin/services/interface.d.ts
@@ -1,27 +1,3 @@
-export interface QrcodeImage {
-  retcode: number;
-  msg: string;
-  data: {
-    qrid: string;
-    image: string;
-  };
-}
-
-export interface QrcodeCheck {
-  retcode: number;
-  msg: string;
-  data: {
-    alt: string;
-  };
-}
-
-export interface LoginReturn {
-  retcode: number;
-  uid: string;
-  nick: string;
-  crossDomainUrlList: Array<string>;
-}
-
 export interface UserInfo {
   ok: number;
   data: {
