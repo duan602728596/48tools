70574780fa5981e0e5c2f8c3e55110df01d0f0c5
段昊辰 <duanhaochen@126.com>
2023-03-06 23:30:02 +0800

    refactor: 调整类型

--- packages/48tools/src/types/antd.d.ts
+++ packages/48tools/src/types/antd.d.ts
@@ -5,10 +5,13 @@
 import type { ReactElement, JSXElementConstructor } from 'react';
 import type { ModalStaticFunctions } from 'antd/es/modal/confirm';
 import type { MessageInstance } from 'antd/es/message/interface';
+import type { NotificationInstance } from 'antd/es/notification/interface';
 
-export type UseModalReturnType = readonly [Omit<ModalStaticFunctions, 'warn'>, ReactElement];
-export type UseMessageReturnType = readonly [MessageInstance, ReactElement<any, string | JSXElementConstructor<any>>];
+type ContextHolderType = ReactElement<any, string | JSXElementConstructor<any>>;
 
+export type UseModalReturnType = readonly [Omit<ModalStaticFunctions, 'warn'>, ContextHolderType];
+export type UseMessageReturnType = readonly [MessageInstance, ContextHolderType];
+export type UseNotificationType = readonly [NotificationInstance, ContextHolderType];
 
 /* select */
 export interface LabeledValue {
