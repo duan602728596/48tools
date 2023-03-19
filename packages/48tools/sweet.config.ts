ee0d3c0d61bd6d3ada633d28194ece08dd1d493f
段昊辰 <duanhaochen@126.com>
2023-03-19 14:54:35 +0800

    build: 移除无用的文件编译

--- packages/48tools/sweet.config.ts
+++ packages/48tools/sweet.config.ts
@@ -109,12 +109,12 @@ export default function(info: object): Record<string, any> {
     javascript: {
       ecmascript: true,
       plugins,
-      exclude: /node_modules|BlythE|toutiaosdk-(acrawler|captcha|webmssdk\.es5)\.js|Signer\.js/i
+      exclude: /node_modules|toutiaosdk-(acrawler|captcha)\.js|Signer\.js/i
     },
     typescript: {
       configFile: isDev ? 'tsconfig.json' : 'tsconfig.prod.json',
       plugins,
-      exclude: /node_modules|BlythE|Signer\.js/
+      exclude: /node_modules|Signer\.js/
     },
     sass: {
       include: /src/
@@ -125,7 +125,7 @@ export default function(info: object): Record<string, any> {
     },
     rules: [
       {
-        test: /toutiaosdk-(acrawler|captcha|webmssdk\.es5)\.js/,
+        test: /toutiaosdk-(acrawler|captcha)\.js/,
         type: 'asset/resource',
         generator: {
           filename: '[name][ext]'
