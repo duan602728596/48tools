807ec698eace64f5da89bb2b03e69be037f46d61
段昊辰 <duanhaochen@126.com>
2023-03-25 00:24:45 +0800

    refactor: 居中和禁止点击蒙层关闭

--- packages/48tools/src/components/AddLiveRoomForm/AddLiveRoomForm.tsx
+++ packages/48tools/src/components/AddLiveRoomForm/AddLiveRoomForm.tsx
@@ -69,6 +69,8 @@ function AddLiveRoomForm(props: AddLiveRoomFormProps): ReactElement {
         open={ visible }
         width={ 500 }
         afterClose={ handleAddModalClose }
+        centered={ true }
+        maskClosable={ false }
         onOk={ handleAddRoomIdClick }
         onCancel={ handleCloseAddModalClick }
       >
