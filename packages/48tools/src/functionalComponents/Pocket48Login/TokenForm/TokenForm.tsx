21f9ebde8c1a638649a04340e2197df809ca019b
段昊辰 <duanhaochen@126.com>
2023-04-11 00:04:07 +0800

    refactor: 添加验证

--- packages/48tools/src/functionalComponents/Pocket48Login/TokenForm/TokenForm.tsx
+++ packages/48tools/src/functionalComponents/Pocket48Login/TokenForm/TokenForm.tsx
@@ -1,6 +1,24 @@
 import type { ReactElement } from 'react';
 import { Form, Input, type FormInstance } from 'antd';
+import type { Rule } from 'rc-field-form/es/interface';
 import style from './tokenForm.sass';
+
+const rules: Array<Rule> = [
+  { required: true, message: '请输入Token', whitespace: false },
+  {
+    async validator(rule: Rule, value: string): Promise<void> {
+      if (value && !/^\s*$/.test(value)) {
+        if (/^[^\r\n\b]+$/.test(value)) {
+          return await Promise.resolve();
+        } else {
+          throw new Error('Token包含换行，请删除');
+        }
+      } else {
+        return await Promise.resolve();
+      }
+    }
+  }
+];
 
 /* 直接输入Token */
 function TokenForm(props: { form: FormInstance }): ReactElement {
@@ -8,7 +26,7 @@ function TokenForm(props: { form: FormInstance }): ReactElement {
 
   return (
     <Form form={ form }>
-      <Form.Item name="token" rules={ [{ required: true, message: '请输入Token', whitespace: false }] }>
+      <Form.Item name="token" rules={ rules }>
         <Input.TextArea className={ style.textarea } rows={ 5 } placeholder="将Token粘贴到这里" />
       </Form.Item>
     </Form>
