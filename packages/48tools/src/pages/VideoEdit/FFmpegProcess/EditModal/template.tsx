fd96d5e8d83cab803324f6b91401d5b39ca09e36
段昊辰 <duanhaochen@126.com>
2023-04-23 00:38:02 +0800

    refactor: 添加新的转码命令

--- packages/48tools/src/pages/VideoEdit/FFmpegProcess/EditModal/template.tsx
+++ packages/48tools/src/pages/VideoEdit/FFmpegProcess/EditModal/template.tsx
@@ -24,6 +24,11 @@ export const template: Array<TplItem> = [
     value: ['-i', Text.Input, '-vcodec', 'libx264', Text.Out].join('  ')
   },
   {
+    id: 'default-3',
+    label: '视频下载并转码到MP4（二）',
+    value: ['-i', Text.Input, '-c:v', 'h264', '-c:a', 'aac', Text.Out].join('  ')
+  },
+  {
     id: 'default-2',
     label: 'M3U8视频格式下载',
     value: ['-protocol_whitelist', '"file,http,https,tcp,tls"', '-i', Text.Input, '-c', 'copy', Text.Out].join('  ')
