73df82e5e90d3c82f0c01c81003203a3cb386920
段昊辰 <duanhaochen@126.com>
2023-04-28 01:03:35 +0800

    refactor: log协议的处理

--- packages/main/src/logProtocol/logProtocol.ts
+++ packages/main/src/logProtocol/logProtocol.ts
@@ -2,7 +2,7 @@ import { protocol, type Protocol, type ProtocolRequest, type ProtocolResponse } 
 import { playUrlLogTemplate } from './logTemplate/bilibiliLive';
 import { utilLogTemplate } from './logTemplate/ffmpeg';
 
-// template方法
+/* template方法 */
 const template: Record<string, Record<string, (t: string, fn: string, d: string) => string>> = {
   bilibililive: {
     playurl: playUrlLogTemplate
@@ -21,39 +21,40 @@ protocol.registerSchemesAsPrivileged([{
   }
 }]);
 
+/* log协议的处理 */
+function registerStringProtocolCallback(request: ProtocolRequest, callback: (response: ProtocolResponse) => void): void {
+  // 解析params
+  const urlResult: URL = new URL(request.url);
+  const type: string | null = urlResult.searchParams.get('type'); // 类型
+  const fn: string | null = urlResult.searchParams.get('fn');     // 执行的方法
+  const data: string | undefined = request?.uploadData?.[0]?.bytes.toString();
+
+  if (!(type && fn && data)) {
+    callback({ statusCode: 404 });
+
+    return;
+  }
+
+  // 生成日志
+  let resData: string | null = null;
+
+  if (template?.[type]?.[fn]) {
+    resData = template?.[type]?.[fn](type, fn, data);
+  }
+
+  if (resData) {
+    callback({ statusCode: 200, data: resData, mimeType: 'text/plain; charset=utf8' });
+  } else {
+    callback({ statusCode: 204 });
+  }
+}
+
 /* 创建协议，拦截日志 */
 function logProtocol(): void {
   const { isProtocolRegistered, registerStringProtocol }: Protocol = protocol;
 
   if (!isProtocolRegistered('log')) {
-    registerStringProtocol(
-      'log',
-      function(request: ProtocolRequest, callback: (response: ProtocolResponse) => void): void {
-        // 解析params
-        const urlResult: URL = new URL(request.url);
-        const type: string | null = urlResult.searchParams.get('type'); // 类型
-        const fn: string | null = urlResult.searchParams.get('fn');     // 执行的方法
-        const data: string | undefined = request?.uploadData?.[0]?.bytes.toString();
-
-        if (!(type && fn && data)) {
-          callback({ statusCode: 404 });
-
-          return;
-        }
-
-        // 生成日志
-        let resData: string | null = null;
-
-        if (template?.[type]?.[fn]) {
-          resData = template?.[type]?.[fn](type, fn, data);
-        }
-
-        if (resData) {
-          callback({ statusCode: 200, data: resData, mimeType: 'text/plain; charset=utf8' });
-        } else {
-          callback({ statusCode: 204 });
-        }
-      });
+    registerStringProtocol('log', registerStringProtocolCallback);
   }
 }
 
