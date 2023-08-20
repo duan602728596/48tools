5b479737d3e751bb857934de4e8860c1a60b822e
段昊辰 <duanhaochen@126.com>
2023-08-20 23:53:09 +0800

    refactor: 测试环境不显示声明

--- packages/48tools/src/pages/Agreement/function/helper.ts
+++ packages/48tools/src/pages/Agreement/function/helper.ts
@@ -5,6 +5,8 @@ const NOW_POWER_VERSION: '20230820' = '20230820';
 
 /* 判断是否需要阅读声明 */
 export function needToReadPower(): boolean {
+  if (globalThis.__INITIAL_STATE__.isTest) return false;
+
   const userTourVersion: string | null = localStorage.getItem(NOW_POWER_VERSION_KEY);
 
   return NOW_POWER_VERSION !== userTourVersion;
