5f2a3ecf495d7db3fb1923f4256b78e1edf4e8b4
Haochen Duan <haochenduan@microsoft.com>
2023-12-18 20:08:14 +0800

    revert: 回退代码

--- packages/test/src/testConfig.ts
+++ packages/test/src/testConfig.ts
@@ -12,9 +12,9 @@ interface TestConfig {
 export const testConfig: TestConfig = {
   bilibili: {
     // 如果为true，会跑港澳台的测试用例
-    useProxy: false,
+    useProxy: true,
     proxy: 'http://127.0.0.1:15732',
-    cookie: '***REMOVED***'
+    cookie: undefined
   },
   douyin: {
     cookie: undefined
