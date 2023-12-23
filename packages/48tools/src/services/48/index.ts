a9ecaca5b94e36eb40ecf45893ef6d2a73009e42
Haochen Duan <haochenduan@microsoft.com>
2023-12-24 01:09:37 +0800

    refactor: 删除无用函数

--- packages/48tools/src/services/48/index.ts
+++ packages/48tools/src/services/48/index.ts
@@ -239,17 +239,6 @@ export async function requestFriendshipAdd(toSourceId: number): Promise<FriendSh
   return res.body;
 }
 
-/**
- * 抓取网页地址
- * TODO: 48的网页会卡下次抓取，所以使用fetch来抓取
- * @param { string } uri: 网页地址
- */
-export async function requestFetchHtml(uri: string): Promise<string> {
-  const res: Response = await fetch(uri);
-
-  return await res.text();
-}
-
 /* 获取公演直播或录播列表 */
 export async function requestOpenLiveList(args: {
   groupId?: number;
