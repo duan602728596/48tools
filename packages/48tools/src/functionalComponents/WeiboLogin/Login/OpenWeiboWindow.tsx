2c0365c941b8da1ba5d4d571f6e4327ff8a1e9c2
duan602728596 <duanhaochen@126.com>
2024-01-10 13:07:35 +0800

    feat: 修改微博登录的逻辑，删除移动端的登录方式

--- packages/48tools/src/functionalComponents/WeiboLogin/Login/OpenWeiboWindow.tsx
+++ packages/48tools/src/functionalComponents/WeiboLogin/Login/OpenWeiboWindow.tsx
@@ -3,13 +3,12 @@ import { Fragment, useEffect, useCallback, type ReactElement, type MouseEvent } 
 import * as PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import type { Dispatch } from '@reduxjs/toolkit';
-import { Button, Alert, Space, message, Divider } from 'antd';
+import { Button, Alert, Space, message } from 'antd';
 import type { UseMessageReturnType } from '@48tools-types/antd';
 import * as dayjs from 'dayjs';
 import { requestUid, requestUserInfo, type UserInfo } from '@48tools-api/weibo/login';
 import { WeiboLoginChannel } from '@48tools/main/src/channelEnum';
 import { IDBSaveAccount } from '../reducers/weiboLogin';
-import UserBrowserLogin from './UserBrowserLogin';
 
 /* 打开微博窗口 */
 function OpenWeiboWindow(props: { onCancel?: Function }): ReactElement {
@@ -19,11 +18,10 @@ function OpenWeiboWindow(props: { onCancel?: Function }): ReactElement {
   // 监听是否登陆
   const handleWeiboLoginCookieListener: (event: IpcRendererEvent, cookies: Array<Cookie>) => Promise<void>
     = useCallback(async function(event: IpcRendererEvent, cookies: Array<Cookie>): Promise<void> {
-      const ssoLoginIndex: number = cookies.findIndex((o: Cookie): boolean => o.name === 'SSOLoginState'),
-        subIndex: number = cookies.findIndex((o: Cookie): boolean => o.name === 'SUB');
+      const subIndex: number = cookies.findIndex((o: Cookie): boolean => o.name === 'SUB');
 
-      if (ssoLoginIndex >= 0 && subIndex >= 0) {
-        const cookieStr: string = `SUB=${ cookies[subIndex].value }`;
+      if (subIndex >= 0) {
+        const cookieStr: string = cookies.map((o: Cookie): string => `${ o.name }=${ o.value }`).join('; ');
         const uid: string | undefined = await requestUid(cookieStr);
 
         if (uid) {
@@ -42,6 +40,8 @@ function OpenWeiboWindow(props: { onCancel?: Function }): ReactElement {
         } else {
           messageApi.error('账号的uid获取失败！');
         }
+      } else {
+        messageApi.error('Cookie获取失败！');
       }
     }, []);
 
@@ -62,10 +62,8 @@ function OpenWeiboWindow(props: { onCancel?: Function }): ReactElement {
     <Fragment>
       <div className="mb-[16px]">
         <Space size={ 8 }>
-          <Alert message="新窗口登陆完毕后关闭窗口，完成登陆。建议无头浏览器登陆。" />
-          <Button onClick={ handleLoginWeiboClick }>微博登陆</Button>
-          <Divider type="vertical" />
-          <UserBrowserLogin onCancel={ props.onCancel } />
+          <Alert message="新窗口登陆完毕后关闭窗口，完成登陆。" />
+          <Button type="primary" onClick={ handleLoginWeiboClick }>微博登陆</Button>
         </Space>
       </div>
       { messageContextHolder }
