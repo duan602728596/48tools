80db7f31c30848b2926547dc7d1c3f5281a61624
Haochen Duan <haochenduan@microsoft.com>
2024-04-07 21:24:52 +0800

    style: 删除单引号

--- packages/48tools/src/services/weibo/live/index.ts
+++ packages/48tools/src/services/weibo/live/index.ts
@@ -11,7 +11,7 @@ export async function requestPcLiveJson(liveId: string): Promise<PcLiveJson> {
   const res: GotResponse<PcLiveJson> = await got.get(`https://weibo.com/l/!/2/wblive/room/show_pc_live.json?live_id=${ liveId }`, {
     responseType: 'json',
     headers: {
-      'Referer': `https://weibo.com/l/wblive/p/show/${ liveId }`
+      Referer: `https://weibo.com/l/wblive/p/show/${ liveId }`
     }
   });
 
