e2e5ef712c046c74dfc00e3c09dc2380e7da8221
Haochen Duan <haochenduan@microsoft.com>
2024-04-08 00:06:03 +0800

    refactor: 更新直播结束状态和回放地址

--- packages/48tools/src/services/weibo/live/interface.d.ts
+++ packages/48tools/src/services/weibo/live/interface.d.ts
@@ -4,7 +4,7 @@ export interface PcLiveJson {
   error_code: number;
   data: {
     liveId: string;
-    status: 1 | number;
+    status: 1 | 3 | number; // 1: 直播中, 3: 直播结束
     cover: string;
     title: string;
     user: {
@@ -13,5 +13,6 @@ export interface PcLiveJson {
     };
     live_origin_hls_url: string;
     live_origin_flv_url: string;
+    replay_origin_url: string;
   };
 }
