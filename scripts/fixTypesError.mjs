76a2dade3b75da00fbe94750efbd1b417d6e0e5f
duan602728596 <duanhaochen@126.com>
2024-05-14 20:10:50 +0800

    fix: 拦截WebSocket的send方法，重写消息中的设备类型和UA

--- scripts/fixTypesError.mjs
+++ scripts/fixTypesError.mjs
@@ -2,11 +2,33 @@ import path from 'node:path';
 import fsP from 'node:fs/promises';
 import { cwd } from './utils.mjs';
 
-async function fixTypesError() {
-  const filePath = path.join(cwd, 'node_modules/@reduxjs/toolkit/src/query/react/module.ts');
-  const file = await fsP.readFile(filePath, { encoding: 'utf8' });
+const nodeModules = path.join(cwd, 'node_modules');
 
-  await fsP.writeFile(filePath, `// @ts-nocheck\n${ file }`, { encoding: 'utf8' });
+async function replaceWebsocket(fp) {
+  const filePath = path.join(nodeModules, fp);
+  const file = await fsP.readFile(filePath, { encoding: 'utf8' });
+  const newFile = file.replace(/window\.WebSocket/g, 'window.WebSocket2||window.WebSocket');
+
+  await fsP.writeFile(filePath, newFile, { encoding: 'utf8' });
+}
+
+async function fixTypesError() {
+  const reduxToolkitFilePath = path.join(nodeModules, '@reduxjs/toolkit/src/query/react/module.ts');
+  const reduxToolkitFile = await fsP.readFile(reduxToolkitFilePath, { encoding: 'utf8' });
+
+  await fsP.writeFile(reduxToolkitFilePath, `// @ts-nocheck\n${ reduxToolkitFile }`, { encoding: 'utf8' });
+
+  // 替换window.WebSocket
+  await Promise.all([
+    replaceWebsocket('nim-web-sdk-ng/dist/NIM_BROWSER_SDK.js'),
+    replaceWebsocket('nim-web-sdk-ng/dist/QCHAT_BROWSER_SDK.js')
+  ]);
+
+  const NIMWebSDKFilePath = path.join(nodeModules, '@yxim/nim-web-sdk/dist/SDK/NIM_Web_SDK.js');
+  const NIMWebSDKFile = await fsP.readFile(NIMWebSDKFilePath, { encoding: 'utf8' });
+  const newNIMWebSDKFile = NIMWebSDKFile.replace(/\.MozWebSocket/g, '.MozWebSocket||window.WebSocket2');
+
+  await fsP.writeFile(NIMWebSDKFilePath, newNIMWebSDKFile, { encoding: 'utf8' });
 }
 
 fixTypesError();
