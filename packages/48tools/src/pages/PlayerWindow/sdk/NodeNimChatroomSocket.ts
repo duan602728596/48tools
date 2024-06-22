79f65fc427a12089318800f8d2880342efb063b9
Haochen Duan <haochenduan@microsoft.com>
2024-06-22 23:07:48 +0800

    refactor: 修改缓存地址

--- packages/48tools/src/pages/PlayerWindow/sdk/NodeNimChatroomSocket.ts
+++ packages/48tools/src/pages/PlayerWindow/sdk/NodeNimChatroomSocket.ts
@@ -29,7 +29,7 @@ class NodeNimChatroomSocket {
     this.account = account; // 账号
     this.token = token;     // token
     this.roomId = roomId;   // 房间id
-    this.appDataDir = path.join(appDataDir, account); // app数据目录
+    this.appDataDir = path.join(appDataDir, '_48tools_node_nim_app_data_', account); // app数据目录
     this.onMessage = onMessage;
   }
 
