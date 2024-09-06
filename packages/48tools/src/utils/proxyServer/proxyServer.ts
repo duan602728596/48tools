b3dbc48c24e55678d671d76018561cff176f32ec
duan602728596 <duanhaochen@126.com>
2024-09-06 13:48:55 +0800

    perf: 添加错误捕捉和lock

--- packages/48tools/src/utils/proxyServer/proxyServer.ts
+++ packages/48tools/src/utils/proxyServer/proxyServer.ts
@@ -3,6 +3,7 @@ import { ProxyServerChannel } from '@48tools/main/src/channelEnum';
 import { detectPort } from '../utils';
 
 let start: boolean = false;
+let startLock: boolean = false;
 
 /* 端口号 */
 export interface ProxyServerPort {
@@ -19,13 +20,21 @@ export function getProxyServerPort(): ProxyServerPort {
 
 /* 启动服务，将rtmp转换成flv */
 export async function proxyServerInit(): Promise<void> {
-  if (start) return;
+  if (start || startLock) return;
 
-  netMediaServerPort.port = await detectPort(netMediaServerPort.port);
+  startLock = true;
 
-  // 等待渲染线程启动后，发送消息到主线程，启动proxy-server服务
-  ipcRenderer.send(ProxyServerChannel.ProxyServer, {
-    port: netMediaServerPort.port
-  });
-  start = true;
+  try {
+    netMediaServerPort.port = await detectPort(netMediaServerPort.port);
+
+    // 等待渲染线程启动后，发送消息到主线程，启动proxy-server服务
+    ipcRenderer.send(ProxyServerChannel.ProxyServer, {
+      port: netMediaServerPort.port
+    });
+    start = true;
+  } catch (err) {
+    console.error(err);
+  }
+
+  startLock = false;
 }
