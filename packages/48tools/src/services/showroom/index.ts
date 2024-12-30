3fda69a234b48385d811373d27cb53ba4e1f6da6
duan602728596 <duanhaochen@126.com>
2024-12-31 00:32:23 +0800

    feat: 添加showroom的接口

---
+++ packages/48tools/src/services/showroom/index.ts
@@ -0,0 +1,31 @@
+// @ts-ignore
+import got, { type Response as GotResponse } from 'got';
+import type { StreamingUrl } from './interface';
+
+export type * from './interface';
+
+/**
+ * 请求html用于解析
+ * @param { string } userId - 用户id
+ * @return { Promise<string> } - 返回html，用于解析直播id
+ */
+export async function requestHtml(userId: string): Promise<string> {
+  const res: GotResponse<string> = await got.get(`https://www.showroom-live.com/r/${ userId }`, {
+    type: 'text'
+  });
+
+  return res.body;
+}
+
+/**
+ * 请求直播地址
+ * @param { string } id - 直播id
+ * @return { Promise<StreamingUrl> } - 返回直播地址，从[1]开始为高画质
+ */
+export async function requestStreamingUrl(id: string): Promise<StreamingUrl> {
+  const res: GotResponse<StreamingUrl> = await got.get(`https://www.showroom-live.com/api/live/streaming_url?room_id=${ id }&abr_available=1`, {
+    responseType: 'json'
+  });
+
+  return res.body;
+}
---
+++ packages/48tools/src/services/showroom/interface.d.ts
@@ -0,0 +1,11 @@
+export interface StreamingUrlItem {
+  id: number;
+  label: 'Automatic' | '普通規格' | 'Medium quality' | '低規格';
+  quality: number;
+  type: 'hls_all' | 'hls' | 'lhls';
+  url: string;
+}
+
+export interface StreamingUrl {
+  streaming_url_list: Array<StreamingUrlItem>;
+}
