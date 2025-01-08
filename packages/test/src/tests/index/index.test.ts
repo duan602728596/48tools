839d20c0fa479dfad3d2131f0ad4fdb6abd46861
duan602728596 <duanhaochen@126.com>
2025-01-08 23:04:15 +0800

    test: 更新测试用例

--- packages/test/src/tests/index/index.test.ts
+++ packages/test/src/tests/index/index.test.ts
@@ -33,7 +33,7 @@ export function callback(): void {
     // 测试导航的正常显示
     const navs: Array<ElementHandleForTag<'nav'>> = await app.win.$$('nav');
 
-    expect(navs.length).toEqual(5);
+    expect(navs.length).toEqual(6);
 
     // 测试二维码图片的正常显示
     const images: Array<ElementHandle> = await app!.win.$$('.ant-image');
