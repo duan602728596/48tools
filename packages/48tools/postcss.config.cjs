b802b9037cba1150b1a7143d0f843425d6e31ea8
段昊辰 <duanhaochen@126.com>
2025-01-26 21:01:08 +0800

    feat: tailwindcss升级到v4

--- packages/48tools/postcss.config.cjs
+++ packages/48tools/postcss.config.cjs
@@ -1,4 +1,4 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require('@tailwindcss/postcss');
 const { default: removeClassnames } = require('@48tools/postcss-plugin-remove-classnames');
 
 module.exports = {
