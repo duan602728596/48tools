435a7789d684afbf6c157d5917b37c9a31844440
段昊辰 <duanhaochen@126.com>
2025-01-28 18:34:00 +0800

    fix: 拼音错误

--- packages/48tools/src/services/xiaohongshu/index.ts
+++ packages/48tools/src/services/xiaohongshu/index.ts
@@ -8,7 +8,7 @@ export type * from './interface';
  * @param { string } roomId - 直播间id
  * @param { string } [dynpath] - 目前发现不影响直播间的请求
  */
-export async function requestXIaohongshuLiveStream(roomId: string, dynpath: string = 'dynpathpYJesOPX'): Promise<string> {
+export async function requestXiaohongshuLiveStream(roomId: string, dynpath: string = 'dynpathpYJesOPX'): Promise<string> {
   const res: GotResponse<string> = await got.get(`https://www.xiaohongshu.com/livestream/${ dynpath }/${ roomId }`, {
     responseType: 'text'
   });
