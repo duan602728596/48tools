f44ff545cc25db7b5d70f92343010d552dbf4ad1
duan602728596 <duanhaochen@126.com>
2025-03-20 20:45:14 +0800

    test: 添加课程的测试用例

--- packages/48tools/src/scrapy/bilibili/BilibiliScrapy.spec.ts
+++ packages/48tools/src/scrapy/bilibili/BilibiliScrapy.spec.ts
@@ -1,15 +1,17 @@
-import type { WebInterfaceViewData, RequestVideoInfoArgumentObject, VideoInfo, AudioInfo, BangumiWebSeason } from '@48tools-api/bilibili/download';
+import type { WebInterfaceViewData, RequestVideoInfoArgumentObject, VideoInfo, AudioInfo, BangumiWebSeason, PugvSeason, PugvSeasonPlayUrl } from '@48tools-api/bilibili/download';
 import * as x$webInterface$view__bvid_BV1og4y187vP from '__mocks__/services/bilibili/x$web-interface$view@bvid=BV1og4y187vP.mockdata.json';
 import * as x$player$playUrl__bvid_BV1og4y187vP_cid_185864709 from '__mocks__/services/bilibili/x$player$playurl@bvid=BV1og4y187vP#cid=185864709.mockdata.json';
 import * as audio$musicServiceC$url__songid_590187 from '__mocks__/services/bilibili/audio$music-service-c$ur@songid=590187.mockdata.json';
 import * as pgc$view$web$season__ep_id_476665 from '__mocks__/services/bilibili/pgc$view$web$season@ep_id=476665.mockdata.json';
 import * as x$player$playUrl__bvid_BV1BP4y1K7cC_cid_566153909 from '__mocks__/services/bilibili/x$player$playurl@bvid=BV1BP4y1K7cC#cid=566153909.mockdata.json';
+import * as pugv$view$web$season__ep_id_215167 from '__mocks__/services/bilibili/pugv$view$web$season@ep_id=215167.mock.json';
+import * as pugv$player$web$playurl__avid_960526794_ep_id_215167_cid_1255148570 from '__mocks__/services/bilibili/pugv$player$web$playurl@avid=960526794#ep_id=215167#cid=1255148570.mockdata.json';
 
 /**
  * 将json的类型转换，修复某些类型对不上的问题
  * @param { any } x
  */
-function jsonTypeTransform<T>(x: any): T {
+function JsonTypeTransform<T>(x: any): T {
   return x;
 }
 
@@ -19,8 +21,10 @@ jest.mock('@48tools-api/bilibili/download', (): {
   requestVideoInfo(x: RequestVideoInfoArgumentObject): Promise<VideoInfo>;
   requestAudioInfo(): Promise<AudioInfo>;
   requestBangumiWebSeason(): Promise<BangumiWebSeason>;
+  requestPugvSeasonV2(): Promise<PugvSeason>;
+  requestPugvPlayurl(): Promise<PugvSeasonPlayUrl>;
 } => ({
-  requestWebInterfaceView: (): Promise<WebInterfaceViewData> => Promise.resolve(jsonTypeTransform<WebInterfaceViewData>(x$webInterface$view__bvid_BV1og4y187vP)),
+  requestWebInterfaceView: (): Promise<WebInterfaceViewData> => Promise.resolve(JsonTypeTransform<WebInterfaceViewData>(x$webInterface$view__bvid_BV1og4y187vP)),
   requestVideoInfo({ type, id, cid, proxy, isDash }: RequestVideoInfoArgumentObject): Promise<VideoInfo> {
     if (id === 'BV1BP4y1K7cC') {
       return Promise.resolve(x$player$playUrl__bvid_BV1BP4y1K7cC_cid_566153909);
@@ -29,10 +33,13 @@ jest.mock('@48tools-api/bilibili/download', (): {
     return Promise.resolve(x$player$playUrl__bvid_BV1og4y187vP_cid_185864709);
   },
   requestAudioInfo: (): Promise<AudioInfo> => Promise.resolve(audio$musicServiceC$url__songid_590187),
-  requestBangumiWebSeason: (): Promise<BangumiWebSeason> => Promise.resolve(pgc$view$web$season__ep_id_476665)
+  requestBangumiWebSeason: (): Promise<BangumiWebSeason> => Promise.resolve(pgc$view$web$season__ep_id_476665),
+  requestPugvSeasonV2: (): Promise<PugvSeason> => Promise.resolve(JsonTypeTransform<PugvSeason>(pugv$view$web$season__ep_id_215167)),
+  requestPugvPlayurl: (): Promise<PugvSeasonPlayUrl> => Promise.resolve(JsonTypeTransform<PugvSeasonPlayUrl>(pugv$player$web$playurl__avid_960526794_ep_id_215167_cid_1255148570))
 }));
 
-import { BilibiliScrapy, BilibiliVideoType, type BilibiliVideoResultItem, type BilibiliVideoInfoItem, type FindVideoInfoReturn } from './BilibiliScrapy';
+import { BilibiliScrapy, BilibiliVideoType, type BilibiliVideoResultItem, type FindVideoInfoReturn } from './BilibiliScrapy';
+import { describe } from 'node:test';
 
 describe('test BilibiliScrapy class', function(): void {
   /* 视频的测试 */
@@ -139,6 +146,41 @@ describe('test BilibiliScrapy class', function(): void {
     test('should parse bangumi when pass url', async function(): Promise<void> {
       const bilibiliScrapy: BilibiliScrapy = new BilibiliScrapy({
         url: 'https://www.bilibili.com/bangumi/play/ep476664?spm_id_from=333.999.0.0&from_spmid=666.25.episode.0'
+      });
+
+      await verify(bilibiliScrapy);
+    });
+  });
+
+  /* 课程的测试 */
+  describe('test parse cheese', function(): void {
+    // 验证
+    async function verify(bilibiliScrapy: BilibiliScrapy): Promise<void> {
+      await bilibiliScrapy.parse();
+      await bilibiliScrapy.asyncLoadVideoInfoByPage();
+
+      const videoResultItem: BilibiliVideoResultItem = bilibiliScrapy.findVideoResult();
+      const { videoInfoItem }: FindVideoInfoReturn = bilibiliScrapy.findVideoInfo();
+
+      expect(bilibiliScrapy.videoResult.length).toBeGreaterThan(0);
+      expect(bilibiliScrapy.title).toBe(pugv$view$web$season__ep_id_215167.data.title);
+      expect(videoResultItem.videoInfo.length).toBeGreaterThan(0);
+      expect(videoResultItem.title).toBe(pugv$view$web$season__ep_id_215167.data.episodes[1].title);
+      expect(videoInfoItem.videoUrl).toBe(pugv$player$web$playurl__avid_960526794_ep_id_215167_cid_1255148570.data.dash.video[2].base_url);
+    }
+
+    test('should parse cheese when pass type and id', async function(): Promise<void> {
+      const bilibiliScrapy: BilibiliScrapy = new BilibiliScrapy({
+        type: BilibiliVideoType.CHEESE_EP,
+        id: '215167'
+      });
+
+      await verify(bilibiliScrapy);
+    });
+
+    test('should parse cheese when pass url', async function(): Promise<void> {
+      const bilibiliScrapy: BilibiliScrapy = new BilibiliScrapy({
+        url: 'https://www.bilibili.com/cheese/play/ep215167?csource=private_space_tougao_null'
       });
 
       await verify(bilibiliScrapy);
